abc607818dcb61b9b1aa6d6368ac1da4f2af1608
Nikotiine <92221729+Nikotiine@users.noreply.github.com>
2026-01-16 13:47:15 +0100

    Fix dto magazine(#13)
    * fix: relation body in new item
    
    * update version

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const config = new DocumentBuilder()
     .setTitle('Gears of shoot')
     .setDescription('Gears of shoot API')
-    .setVersion('0.4.0')
+    .setVersion('0.4.2')
     .addBearerAuth(
       {
         type: 'http',
